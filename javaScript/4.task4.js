7bda1241e11d41792781aacd325b0bf2006cf9a8
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-05 21:39:05 +0530

    isnan check

---
+++ javaScript/4.task4.js
@@ -0,0 +1,14 @@
+/*
+Task-4
+What will be the result of the following codes:
+
+var a = isNaN(‘11’);
+
+var a = isNaN(2-10);
+*/
+
+let a = isNaN("11");
+console.log(a);
+
+let b = isNaN(2 - 10);
+console.log(b);
